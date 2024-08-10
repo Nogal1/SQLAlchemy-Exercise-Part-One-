cd82d09859b8f45c94deed274b3e26db591c31a0
Nogal1 <mcclendon.logan1234@gmail.com>
2024-08-10 12:00:12 -0700

    Refactored, still not runnning

--- app.py
+++ app.py
@@ -1,101 +1,79 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, render_template, redirect, flash, session, url_for
+from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///blogly"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'ihaveasecret'
 
-# Having the Debug Toolbar show redirects explicitly is often useful;
-# however, if you want to turn it off, you can uncomment this line:
-#
-# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///your_database_name'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "your_secret_key"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 toolbar = DebugToolbarExtension(app)
-
 
 connect_db(app)
 db.create_all()
 
-
 @app.route('/')
 def root():
-    """Homepage redirects to list of users."""
-
-    return redirect("/users")
-
-
-##############################################################################
-# User route
+    """Redirect to the list of users."""
+    return redirect(url_for('list_users'))
 
 @app.route('/users')
-def users_index():
-    """Show a page with info on all users"""
-
-    users = User.query.order_by(User.last_name, User.first_name).all()
+def list_users():
+    """List all users."""
+    users = User.query.all()
     return render_template('users/index.html', users=users)
 
+@app.route('/users/new', methods=["GET", "POST"])
+def add_user():
+    """Show form to add a new user and process the form."""
+    if request.method == "POST":
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        image_url = request.form.get('image_url') or None
 
-@app.route('/users/new', methods=["GET"])
-def users_new_form():
-    """Show a form to create a new user"""
+        new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash(f"User {new_user.full_name} added.")
+        return redirect(url_for('list_users'))
 
     return render_template('users/new.html')
 
-
-@app.route("/users/new", methods=["POST"])
-def users_new():
-    """Handle form submission for creating a new user"""
-
-    new_user = User(
-        first_name=request.form['first_name'],
-        last_name=request.form['last_name'],
-        image_url=request.form['image_url'] or None)
-
-    db.session.add(new_user)
-    db.session.commit()
-
-    return redirect("/users")
-
-
 @app.route('/users/<int:user_id>')
-def users_show(user_id):
-    """Show a page with info on a specific user"""
-
+def show_user(user_id):
+    """Show details of a single user."""
     user = User.query.get_or_404(user_id)
     return render_template('users/show.html', user=user)
 
-
-@app.route('/users/<int:user_id>/edit')
-def users_edit(user_id):
-    """Show a form to edit an existing user"""
-
+@app.route('/users/<int:user_id>/edit', methods=["GET", "POST"])
+def edit_user(user_id):
+    """Show form to edit a user and process the form."""
     user = User.query.get_or_404(user_id)
+
+    if request.method == "POST":
+        user.first_name = request.form['first_name']
+        user.last_name = request.form['last_name']
+        user.image_url = request.form.get('image_url') or None
+
+        db.session.commit()
+        flash(f"User {user.full_name} updated.")
+        return redirect(url_for('list_users'))
+
     return render_template('users/edit.html', user=user)
 
-
-@app.route('/users/<int:user_id>/edit', methods=["POST"])
-def users_update(user_id):
-    """Handle form submission for updating an existing user"""
-
-    user = User.query.get_or_404(user_id)
-    user.first_name = request.form['first_name']
-    user.last_name = request.form['last_name']
-    user.image_url = request.form['image_url']
-
-    db.session.add(user)
-    db.session.commit()
-
-    return redirect("/users")
-
-
 @app.route('/users/<int:user_id>/delete', methods=["POST"])
-def users_destroy(user_id):
-    """Handle form submission for deleting an existing user"""
-
+def delete_user(user_id):
+    """Delete a user."""
     user = User.query.get_or_404(user_id)
     db.session.delete(user)
     db.session.commit()
 
-    return redirect("/users")
+    flash(f"User {user.full_name} deleted.")
+    return redirect(url_for('list_users'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- models.py
+++ models.py
@@ -1,34 +1,23 @@
-"""SQLAlchemy models for blogly."""
-
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
-
+def connect_db(app):
+    """Connect to database."""
+    db.app = app
+    db.init_app(app)
 
 class User(db.Model):
-    """Site user."""
+    """User model."""
 
-    __tablename__ = "users"
+    __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.Text, nullable=False)
-    last_name = db.Column(db.Text, nullable=False)
-    image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    image_url = db.Column(db.String, nullable=True, default='')
 
     @property
     def full_name(self):
-        """Return full name of user."""
-
+        """Return full name of the user."""
         return f"{self.first_name} {self.last_name}"
-
-
-def connect_db(app):
-    """Connect this database to provided Flask app.
-
-    You should call this in your Flask app.
-    """
-
-    db.app = app
-    db.init_app(app)
